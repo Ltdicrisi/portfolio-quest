b403993349f54fa052f112ce624b671a3644dac0
LaurenDiCrisi <ryandicrisi@ryans-MacBook-Pro-2.local>
2025-05-14 23:47:41 -0400

    Finalized scroll. Ready for deployment.

---
+++ src/components/About.jsx
@@ -0,0 +1,18 @@
+function About() {
+    return (
+        <section className="about">
+            <h2>About Me</h2>
+            <p>
+        Hail! I am <strong>Lauren “Emerald Hawthorne” Di Crisi</strong>, a Druid of the Digital Realm.
+        I began my journey in the year of 2023, forging runes in HTML, CSS, JavaScript, and React.
+        Formerly a master of inks and wallpapers, I now seek to bend the web to my will as a full stack developer.
+      </p>
+      <p>
+        I specialize in weaving magic through the MERN stack, taming databases, and crafting elegant user interfaces.
+        With my trusty Familiar, Yang, and my party of curious cats and code-wielders, I fight for clean code, accessibility, and tech justice!
+      </p>
+        </section>
+    );
+}
+
+export default About;
---
+++ src/components/Projects.jsx
@@ -0,0 +1,36 @@
+function Projects (){
+    return (
+        <section className="projects">
+            <h2>Relics of My Journey</h2>
+            <div className="project-cards">
+
+                {/* Project 1 */}
+                <div className="project-card">
+                    <h3>Work Order Tracker</h3>
+                    <p>
+                        A fullstack web app forged in React, Node, and MongoDB- used to track and manage production floor tasks in real time.
+                    </p>
+                    <p><strong>Tech:</strong>React, Express, Node.js, MongoDB </p>
+                    <a href=" https://github.com/Ltdicrisi/work-order-tracker" target="_blank" >View on GitHub </a>
+                </div>
+
+                {/* Project 2 */}
+                <div className="project-card">
+                    <h3>Portfolio Quest</h3>
+                    <p>
+                        This very site! A showcases of skills and story, crafted in Vite+ React and styles with whimsy.
+                    </p>
+                    <p>
+                    <strong>
+                        Tech:
+                    </strong>
+                    Vite, React, CSS
+                    </p>
+                    <a href="https://github.com/Ltdicrisi/portfolio-quest" target="_blank">View on GitHub</a>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Projects;
