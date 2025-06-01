cda11a75865e8b125c8e7b2039fa968290e6bd2f
Lauren Di Crisi <Ltdicrisi@yahoo.com>
2025-06-01 14:44:35 -0400

    Fixed file structure, extra syntax, css

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,20 @@ export default function Contact() {
       <section className="contact">
         <h2>Contact</h2>
         <p>Reach me via email, GitHub, or raven if enchanted.</p>
-      </section>
+      <form className="contact-form">
+  <label>Name</label>
+  <input type="text" name="name" required />
+
+  <label>Email</label>
+  <input type="email" name="email" required />
+
+  <label>Message</label>
+  <textarea name="message" rows="5" required></textarea>
+
+  <button type="submit">Send Message</button>
+</form>
+
+     </section>
     );
   }
-  
+
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import "./Hero.css";
-import React from "react";
-import "../Hero.css";
 
 function Hero() {
   return (
     <section className="hero-section">
-      <img src= "/public/herobg.webp" alt="Library background" className="hero-bg" />
+      <img src= "/hero-bg.webp" alt="Library background" className="hero-bg" />
       <div className="hero-overlay">
-        <img src="/public/waxforge.webp" alt="Wax Seal Logo" className="hero-logo" />
+        <img src="/waxSeal.webp" alt="Wax Seal Logo" className="hero-logo" />
         
         <h1 className="greeting">Welcome, Traveler...</h1>
 
