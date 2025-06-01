3a8a4d80210f61bb1432661a75df9ca5c59ad26d
Lauren Di Crisi <Ltdicrisi@yahoo.com>
2025-06-01 15:19:37 -0400

    Img file replaced for Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,9 +4,9 @@ import "./Hero.css";
 function Hero() {
   return (
     <section className="hero-section">
-      <img src= "/hero-bg.webp" alt="Library background" className="hero-bg" />
+      <img src= "/herobg.webp" alt="Library background" className="hero-bg" />
       <div className="hero-overlay">
-        <img src="/waxSeal.webp" alt="Wax Seal Logo" className="hero-logo" />
+        <img src="/waxforge.webp" alt="Wax Seal Logo" className="hero-logo" />
         
         <h1 className="greeting">Welcome, Traveler...</h1>
 
