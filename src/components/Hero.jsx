fc737a282d517be8d4d86c2c82ac7b4e22d257af
Lauren Di Crisi <Ltdicrisi@yahoo.com>
2025-06-01 03:44:40 -0400

    moving files

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import "./Hero.css";
 import React from "react";
-import waxSeal from "/assets/images/waxSeal.webp"; // use /public folder path
-import heroBg from "/assets/images/hero-bg.webp";  // same here
 import "../Hero.css";
 
 function Hero() {
   return (
     <section className="hero-section">
-      <img src={heroBg} alt="Library background" className="hero-bg" />
+      <img src= "/public/herobg.webp" alt="Library background" className="hero-bg" />
       <div className="hero-overlay">
-        <img src={waxSeal} alt="Wax Seal Logo" className="hero-logo" />
+        <img src="/public/waxforge.webp" alt="Wax Seal Logo" className="hero-logo" />
         
         <h1 className="greeting">Welcome, Traveler...</h1>
 
