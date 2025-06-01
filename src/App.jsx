7188b00504c6a9e7cec8591030000a468fdad753
Lauren Di Crisi <Ltdicrisi@yahoo.com>
2025-05-31 22:49:14 -0400

    feat: Added Hero, logo, and organized components     modified:   src/App.jsx 	renamed:    src/assets/email.png -> src/assets/icons/email.png 	renamed:    src/assets/house.png -> src/assets/icons/house.png 	renamed:    src/assets/id-card.png -> src/assets/icons/id-card.png 	renamed:    src/assets/project.png -> src/assets/icons/project.png 	new file:   src/assets/waxSeal.webp 	new file:   src/components/BookCard.jsx 	new file:   src/components/Certifications.jsx 	new file:   src/components/Hero.jsx 	new file:   src/components/Logo.jsx 	new file:   src/components/NavDrawer.jsx 	new file:   src/components/Techstack.jsx 	new file:   src/components/resume.jsx 	modified:   src/components/stylesheet.css 	modified:   src/index.css

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,28 @@ import Projects from "./components/Projects";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Contact from "./components/Contact";
+import './src/App.css';
+import './components/stylesheet.css';
+import Hero from './components/hero';
 
 function App() {
 return(
-  <div>
+  <div className="app-container">
     <Navbar />
-    <h1 style={{color: 'white'}}>Lauren Hawthorne's Portfolio</h1>
+    <main>
+      <section id="hero">
+    <h1 className="hero-title">Lauren DiCrisi's Portfolio</h1>
+    </section>
+    <section className="about">
     <About />
-    <Projects />
+    </section>
+    <section className="projects">
+   <Projects />
+   </section>
+   <section className="contact">
     <Contact />
+    </section>
+    </main>
     <Footer />
   </div>
 );
